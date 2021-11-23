e33c063411cfc141915f990b018e4c5fb47ec186
Gapine <94353794+Gapine@users.noreply.github.com>
2021-11-23 09:24:04 +0100

    Update test_1.ino

--- test_1.ino
+++ test_1.ino
@@ -21,6 +21,24 @@
 
 #include <Adafruit_NeoPixel.h>
 
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1:
+#define LED_PIN    
+
+// How many NeoPixels are attached to the Arduino?
+#define LED_COUNT 10
+
+// Declare our NeoPixel strip object:
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_RGB + NEO_KHZ800);
+// Argument 1 = Number of pixels in NeoPixel strip
+// Argument 2 = Arduino pin number (most are valid)
+// Argument 3 = Pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+
 int Reset = 13;
 int Strobe = 12;
 int AnalogPin = A0;
@@ -30,7 +48,7 @@ void setupRGB()
 {
   strip.begin();
   strip.show();
-  strip.setPixelColor(n, red, green, blue);
+  strip.setPixelColor(9, red, green, blue);
   
   //n = antal pixel 
 }
